a25bd31d317719627f90ea6a38e718842d45a2c4
john <john970473@gmail.com>
2018-04-02 15:31:24 +0800

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+    var el = document.querySelector('h1');
+    el.textContent = 'Guess Number Javascript';
+}
+window.onload = function(){
+    var ans = 24;
+    do{
+        var num = Number(prompt('Please Guess a number from 1 to 1000'));
+        if (num < ans){
+            alert('It is too small');
+        }
+        else if (num > ans){
+            alert('It is too big');
+        }
+        else{
+          var el = document.querySelector('h1');
+          el.textContent = 'Bingo!!';
+        }
+    }while (ans != num);
+}
